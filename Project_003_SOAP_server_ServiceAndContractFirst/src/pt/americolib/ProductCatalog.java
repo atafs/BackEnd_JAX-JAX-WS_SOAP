101de9331ed907d1d84fac77d8b3c6a1280ba5bf
atafs <atafs@iscte-iul.pt>
2016-01-26 12:40:08 +0000

    update

--- Project_003_SOAP_server_ServiceAndContractFirst/src/pt/americolib/ProductCatalog.java
+++ Project_003_SOAP_server_ServiceAndContractFirst/src/pt/americolib/ProductCatalog.java
@@ -7,23 +7,31 @@ import javax.jws.WebService;
 
 import pt.americolib.business.ProductServiceImpl;
 
-@WebService
+@WebService(
+		//definitions tag in wsdl xml
+		name="TestMartCatalog", 
+		portName="TestMartCatalogPort", 
+		serviceName="ProductCatalogService", 
+		targetNamespace="http://americolib.itworld.com/"
+		)
 public class ProductCatalog {
 	
 	//ATTRIBUTES
 	ProductServiceImpl productService = new ProductServiceImpl();
 	
 	//METHOD (public = default @WebMethod)
-	@WebMethod
+	@WebMethod(action="fetch_categories", operationName="fetchCategories")
 	public List<String> getProductCategories() {
 		return productService.getProductCategories();
 	}
 	
+	//EXCLUDE WEB SERVICE METHOD: exclude=true
+//	@WebMethod(exclude=true)
 	public List<String> getProducts(String category) {
 		return productService.getProducts(category);
 	}
 	
-	@WebMethod
+//	@WebMethod(exclude=true)
 	public boolean addProduct(String category, String product) {
 		return productService.addProduct(category, product);
 	}
