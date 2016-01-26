cbe466f49943ca29ae163ffcb1e3b803020dbd33
atafs <atafs@iscte-iul.pt>
2016-01-26 11:11:15 +0000

    PROJECT: Project_002_SOAP_server_addingInputArgs

---
+++ bat/SERVER_glassfish/run1StartServer.bat
@@ -0,0 +1,11 @@
+REM GO TO PATH
+cd D:\glassfish SERVER\glassfish-4.1\bin
+
+REM 
+echo STARTING WITH SUCCESS!!
+
+REM SLEEP
+TIMEOUT /T 4
+
+REM START GLASSFISH SERVER
+asadmin start-domain
---
+++ bat/SERVER_glassfish/run2StopServer.bat
@@ -0,0 +1,12 @@
+REM GO TO PATH
+cd D:\glassfish SERVER\glassfish-4.1\bin
+
+REM 
+echo ENDEDING WITH SUCCESS!!
+
+REM SLEEP
+TIMEOUT /T 4
+
+REM STOP GLASSFISH SERVER
+asadmin stop-domain
+
